8234994f91ba507f464c30e4c08ea67f8610ae23
5h4r3r <fanman45@gmail.com>
2020-11-10 22:52:16 +0500

    10.11

--- ddc/bboard/models.py
+++ ddc/bboard/models.py
@@ -16,8 +16,10 @@ class Bb(models.Model):
 
 class Rubric(models.Model):
     name = models.CharField(max_length=20, db_index=True, verbose_name='Название')
-    
+    def __str__(self):
+        return self.name
+
     class Meta:
-        verbose_name = 'Рубрики'
-        verbose_name_plural = 'Рубрика'
+        verbose_name = 'Рубрика'
+        verbose_name_plural = 'Рубрики'
         ordering = ['name']
