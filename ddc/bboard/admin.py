037299d889f6c92ef754724801a8263796a80f96
5h4r3r <43179851+5h4r3r@users.noreply.github.com>
2020-11-15 21:25:30 +0500

    15.11

--- ddc/bboard/admin.py
+++ ddc/bboard/admin.py
@@ -1,15 +1,17 @@
+from .models import Rubric
 from django.contrib import admin
 
 # Register your models here.
 
 from .models import Bb
+
+
 class BbAdmin(admin.ModelAdmin):
-    list_display = ('title', 'content', 'prise', 'published')
+    list_display = ('title', 'content', 'prise', 'rubric', 'published')
     list_display_links = ('title', 'content')
-    search_fields = ('title','content')
+    search_fields = ('title', 'content')
+
 
 admin.site.register(Bb, BbAdmin)
 
-from .models import Rubric
-
-admin.site.register (Rubric)
+admin.site.register(Rubric)
